add51e8aeee951b14585c784a85b4ad0fc8b8593
Anastasia <nastay.dr2002@gmail.com>
2023-04-05 13:15:00 +0400

    Initial commit

---
+++ src/main/java/com/example/team5animalsheltertelegrambot/Team5AnimalShelterTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.team5animalsheltertelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team5AnimalShelterTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team5AnimalShelterTelegramBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/team5animalsheltertelegrambot/Team5AnimalShelterTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.team5animalsheltertelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team5AnimalShelterTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
