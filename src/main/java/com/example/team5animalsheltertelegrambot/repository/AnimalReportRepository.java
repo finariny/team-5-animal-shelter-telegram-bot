211b7f46d200d496390f00ee2e86e4a55d39befa
rafaelovna <aikanush777@mail.ru>
2023-04-20 20:15:47 +0400

    Добавила поля в AnimalReport. Написала тесты к некоторым методам. Создала класс exception пока временно пустой.

--- src/main/java/com/example/team5animalsheltertelegrambot/repository/AnimalReportRepository.java
+++ src/main/java/com/example/team5animalsheltertelegrambot/repository/AnimalReportRepository.java
@@ -1,9 +1,16 @@
 package com.example.team5animalsheltertelegrambot.repository;
 
-import com.example.team5animalsheltertelegrambot.entity.AnimalReport;
+import com.example.team5animalsheltertelegrambot.entity.report.AnimalReport;
+import com.pengrad.telegrambot.model.File;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface AnimalReportRepository extends JpaRepository<AnimalReport, Integer> {
+
 }
