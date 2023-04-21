e3aa6885af92fb86fbb5b1230851577a0ed71d9d
Anastasia <nastay.dr2002@gmail.com>
2023-04-21 20:56:36 +0400

    Добавила репозитории и сервисы для кошек и собак (убрала общие репозиторий и сервис для животных)

--- src/main/java/com/example/team5animalsheltertelegrambot/controller/animal/DogController.java
+++ src/main/java/com/example/team5animalsheltertelegrambot/controller/animal/DogController.java
@@ -1,7 +1,7 @@
 package com.example.team5animalsheltertelegrambot.controller.animal;
 
 import com.example.team5animalsheltertelegrambot.entity.animal.Dog;
-import com.example.team5animalsheltertelegrambot.service.AnimalService;
+import com.example.team5animalsheltertelegrambot.service.animal.DogService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -14,7 +14,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 /**
  * Контроллер для работы с собаками
@@ -33,10 +32,10 @@ import java.util.Optional;
 })
 public class DogController {
 
-    private final AnimalService<Dog> animalService;
+    private final DogService dogService;
 
-    public DogController(AnimalService<Dog> animalService) {
-        this.animalService = animalService;
+    public DogController(DogService dogService) {
+        this.dogService = dogService;
     }
 
     @Operation(
@@ -54,7 +53,7 @@ public class DogController {
     })
     @PostMapping
     public ResponseEntity<Boolean> save(@RequestBody Dog dog) {
-        boolean isDogSaved = animalService.save(dog);
+        boolean isDogSaved = dogService.save(dog);
         if (isDogSaved) {
             return ResponseEntity.ok().build();
         }
@@ -79,12 +78,8 @@ public class DogController {
             )
     })
     @GetMapping("/{id}")
-    public ResponseEntity<Optional<Dog>> findById(@PathVariable Integer id) {
-        Optional<Dog> dog = animalService.findById(id);
-        if (dog.isEmpty()) {
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(dog);
+    public ResponseEntity<Dog> findById(@PathVariable Integer id) {
+        return ResponseEntity.of(dogService.findById(id));
     }
 
     @Operation(
@@ -110,23 +105,24 @@ public class DogController {
             )
     })
     @GetMapping("/filter")
-    public ResponseEntity<List<Dog>> findAllByHealthAndVaccination(@RequestParam(required = false) Boolean isHealthy, @RequestParam(required = false) Boolean isVaccinated) {
+    public ResponseEntity<List<Dog>> findAllByHealthAndVaccination(@RequestParam(required = false) Boolean isHealthy,
+                                                                   @RequestParam(required = false) Boolean isVaccinated) {
         if (isHealthy != null && isVaccinated != null) {
-            List<Dog> listOfDogs = animalService.findAllByHealthAndVaccination(isHealthy, isVaccinated);
+            List<Dog> listOfDogs = dogService.findAllByHealthAndVaccination(isHealthy, isVaccinated);
             if (listOfDogs.isEmpty()) {
                 return ResponseEntity.notFound().build();
             }
             return ResponseEntity.ok(listOfDogs);
         }
         if (isHealthy != null) {
-            List<Dog> listOfDogs = animalService.findAllByHealth(isHealthy);
+            List<Dog> listOfDogs = dogService.findAllByHealth(isHealthy);
             if (listOfDogs.isEmpty()) {
                 return ResponseEntity.notFound().build();
             }
             return ResponseEntity.ok(listOfDogs);
         }
         if (isVaccinated != null) {
-            List<Dog> listOfDogs = animalService.findAllByVaccinate(isVaccinated);
+            List<Dog> listOfDogs = dogService.findAllByVaccinate(isVaccinated);
             if (listOfDogs.isEmpty()) {
                 return ResponseEntity.notFound().build();
             }
@@ -154,7 +150,7 @@ public class DogController {
     })
     @GetMapping
     public ResponseEntity<List<Dog>> findAll() {
-        List<Dog> listOfDogs = animalService.findAll();
+        List<Dog> listOfDogs = dogService.findAll();
         if (listOfDogs.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
@@ -180,7 +176,7 @@ public class DogController {
                                           @RequestParam Integer age,
                                           @RequestParam Boolean isHealthy,
                                           @RequestParam Boolean isVaccinated) {
-        Integer number = animalService.updateById(id, name, age, isHealthy, isVaccinated);
+        Integer number = dogService.updateById(id, name, age, isHealthy, isVaccinated);
         if (number == 0) {
             return ResponseEntity.badRequest().build();
         }
@@ -202,7 +198,7 @@ public class DogController {
     })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteById(@PathVariable Integer id) {
-        boolean isDogDeleted = animalService.deleteById(id);
+        boolean isDogDeleted = dogService.deleteById(id);
         if (isDogDeleted) {
             return ResponseEntity.ok().build();
         }
---
+++ src/main/java/com/example/team5animalsheltertelegrambot/service/animal/DogService.java
@@ -0,0 +1,137 @@
+package com.example.team5animalsheltertelegrambot.service.animal;
+
+import com.example.team5animalsheltertelegrambot.entity.animal.Dog;
+import com.example.team5animalsheltertelegrambot.repository.animal.DogRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Сервис для работы с собаками
+ */
+
+@Service
+public class DogService {
+    private final DogRepository dogRepository;
+
+    public DogService(DogRepository dogRepository) {
+        this.dogRepository = dogRepository;
+    }
+
+    /**
+     * Сохраняет заданную сущность.
+     * Используется метод репозитория {@link JpaRepository#save(Object)}
+     *
+     * @param dog сохраняемая сущность
+     * @return {@code true} - сущность сохранена, {@code false} - сущность не сохранена
+     */
+    public boolean save(Dog dog) {
+        if (!(dog == null
+                || (dog.getName() == null || dog.getName().isBlank())
+                || (dog.getAge() == null || dog.getAge() < 0)
+                || dog.getHealthy() == null
+                || dog.getVaccinated() == null)) {
+            dogRepository.save(dog);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Возвращает сущность по её идентификатору.
+     * Используется метод репозитория {@link JpaRepository#findById(Object)}
+     *
+     * @param id идентификатор сущности
+     * @return возвращаемая сущность
+     */
+    public Optional<Dog> findById(Integer id) {
+        return dogRepository.findById(id);
+    }
+
+    /**
+     * Возвращает список сущностей по значению состояния здоровья.
+     * Используется метод репозитория {@link DogRepository#findAllByHealth(Boolean)}
+     *
+     * @param isHealthy состояние здоровья ({@code true} - здоров, {@code false} - не здоров)
+     * @return список возвращаемых сущностей
+     */
+    public List<Dog> findAllByHealth(Boolean isHealthy) {
+        return dogRepository.findAllByHealth(isHealthy);
+    }
+
+    /**
+     * Возвращает список сущностей по значению наличия/отсутствия вакцинации.
+     * Используется метод репозитория {@link DogRepository#findAllByVaccination(Boolean)}
+     *
+     * @param isVaccinated наличие вакцинации ({@code true} - вакцинирован, {@code false} - не вакцинирован)
+     * @return список возвращаемых сущностей
+     */
+    public List<Dog> findAllByVaccinate(Boolean isVaccinated) {
+        return dogRepository.findAllByVaccination(isVaccinated);
+    }
+
+    /**
+     * Возвращает список сущностей по значению состояния здоровья и/или значению наличия/отсутствия вакцинации.
+     * Используется метод репозитория {@link DogRepository#findAllByHealthAndVaccination(Boolean, Boolean)}
+     *
+     * @param isHealthy    состояние здоровья ({@code true} - здоров, {@code false} - не здоров)
+     * @param isVaccinated наличие вакцинации ({@code true} - вакцинирован, {@code false} - не вакцинирован)
+     * @return список возвращаемых сущностей
+     */
+    public List<Dog> findAllByHealthAndVaccination(Boolean isHealthy, Boolean isVaccinated) {
+        return dogRepository.findAllByHealthAndVaccination(isHealthy, isVaccinated);
+    }
+
+    /**
+     * Возвращает список всех сущностей.
+     * Используется метод репозитория {@link JpaRepository#findAll()}
+     *
+     * @return список возвращаемых сущностей
+     */
+    public List<Dog> findAll() {
+        return dogRepository.findAll();
+    }
+
+    /**
+     * Обновляет сущность по передаваемым параметрам.
+     * Используется метод репозитория {@link DogRepository#updateById(Integer, String, Integer, Boolean, Boolean)}
+     *
+     * @param id           идентификатор сущности
+     * @param name         имя сущности
+     * @param age          возраст сущности
+     * @param isHealthy    состояние здоровья сущности ({@code true} - здоров, {@code false} - не здоров)
+     * @param isVaccinated наличие вакцинации сущности ({@code true} - вакцинирован, {@code false} - не вакцинирован)
+     * @return число ({@code 1} - сущность обновлена, {@code 0} - сущность не обновлена)
+     */
+    public Integer updateById(Integer id, String name, Integer age, Boolean isHealthy, Boolean isVaccinated) {
+        Optional<Dog> dog = findById(id);
+        if (dog.isEmpty()
+                || (name == null || name.isBlank())
+                || (age == null || age < 0)
+                || isHealthy == null
+                || isVaccinated == null) {
+            return 0;
+        } else {
+            dogRepository.updateById(id, name, age, isHealthy, isVaccinated);
+            return 1;
+        }
+    }
+
+    /**
+     * Удаляет сущность по её идентификатору.
+     * Используется метод репозитория {@link JpaRepository#deleteById(Object)}
+     *
+     * @param id идентификатор удаляемой сущности
+     * @return {@code true} - сущность сохранена, {@code false} - сущность не сохранена
+     */
+    public Boolean deleteById(Integer id) {
+        Optional<Dog> findTById = findById(id);
+        if (id == null || findTById.isEmpty()) {
+            return false;
+        }
+        dogRepository.deleteById(id);
+        return true;
+    }
+}
