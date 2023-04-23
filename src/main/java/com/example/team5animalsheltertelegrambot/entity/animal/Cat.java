644c2a15b4053fd08c5d16369bc8587daf9f9dcd
Anastasia <nastay.dr2002@gmail.com>
2023-04-23 17:38:54 +0400

    Добавила связи с приютами в классы Cat и Dog

--- src/main/java/com/example/team5animalsheltertelegrambot/entity/animal/Cat.java
+++ src/main/java/com/example/team5animalsheltertelegrambot/entity/animal/Cat.java
@@ -1,16 +1,19 @@
 package com.example.team5animalsheltertelegrambot.entity.animal;
 
+import com.example.team5animalsheltertelegrambot.entity.shelter.CatShelter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @NoArgsConstructor
 @Entity
 @DiscriminatorValue(value = "CAT")
 @Table(name = "cat")
 public class Cat extends Animal{
+
+    @ManyToOne
+    @JoinColumn(name = "cats", referencedColumnName = "id")
+    private CatShelter catShelter;
 
     public Cat(String name, Integer age, Boolean isHealthy, Boolean isVaccinated) {
         setName(name);
